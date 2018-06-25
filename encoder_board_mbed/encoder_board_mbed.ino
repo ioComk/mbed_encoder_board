767682a242fabb4e2f78aed1e9f1cb272d210af8
ioComk <rw110407@gmail.com>
2018-06-25 15:00:05 +0900

    add comment

--- encoder_board_mbed/encoder_board_mbed.ino
+++ encoder_board_mbed/encoder_board_mbed.ino
@@ -11,12 +11,13 @@ void setup() {
   Wire.setClock(400000UL);
   Serial.begin(115200);
 
-  enc.init();
+  enc.init();   //エンコーダ基盤の初期化
 }
 
 void loop() {
-  
-  Serial.println(enc.getCount(1));
+
+  //port1に接続されているエンコーダの値を表示
+  Serial.println(enc.getCount(1));  
 
 
   enc.Update();
